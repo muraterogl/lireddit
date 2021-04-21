805c414f1df50fa70968d64d908eb8296048580d
Murat Eroglu <murat51392@gmail.com>
2021-04-21 14:57:34 +0300

    ssr not working

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { Formik, Form } from "formik";
-import { FormControl } from "@chakra-ui/form-control";
-import {
-    Box,
-    Button,
-    FormErrorMessage,
-    FormLabel,
-    Input,
-} from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 interface registerProps {}
 
@@ -62,4 +57,4 @@ const Register: React.FC<registerProps> = ({}) => {
     );
 };
 
-export default Register;
+export default withUrqlClient(createUrqlClient)(Register);
